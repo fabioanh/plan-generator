31519caef915aad7f40f1970ee61d6f2bceb976a
Fabio Navarrete <fabioanh@gmail.com>
2021-02-09 03:01:19 +0400

    Final details for business logic
    Added considerations for last payment

--- src/main/java/de/lendico/plangenerator/PlanService.java
+++ src/main/java/de/lendico/plangenerator/PlanService.java
@@ -13,15 +13,13 @@ import java.util.List;
  */
 @Service
 public class PlanService {
-    private static final Integer SHORT_SCALE = 2;
-    private static final Integer LONG_SCALE = 5;
     // Rounding mode to be used in all operations
     private static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_DOWN;
     public static final int YEARLY_PERIODS = 12;
-
-
+    private static final Integer SHORT_SCALE = 2;
     // Number of periods - 12 as the plan is expected to be used with its monthly values
-    private final BigDecimal numPeriods = BigDecimal.valueOf(12).setScale(2, ROUNDING_MODE);
+    private static final BigDecimal NUM_PERIODS = BigDecimal.valueOf(YEARLY_PERIODS).setScale(2, ROUNDING_MODE);
+
 
     /**
      * Creates a payment plan with monthly values. Relays on the fact that <b>a year has 360 days and each month has 30 days<b/>
@@ -36,7 +34,7 @@ public class PlanService {
                                                      Integer duration, LocalDate startDate) {
         List<BorrowerPayment> result = new ArrayList<>();
         // Simplified rate counting on the fact that a year has 360 days and each month has 30 days
-        final BigDecimal monthlyRate = nominalRate.divide(numPeriods, LONG_SCALE, ROUNDING_MODE).divide(BigDecimal.valueOf(100));
+        final BigDecimal monthlyRate = nominalRate.divide(NUM_PERIODS, 5, ROUNDING_MODE).divide(BigDecimal.valueOf(100));
         final BigDecimal annuity = calculateAnnuity(loanAmount, monthlyRate, duration);
         LocalDate periodDate = startDate;
         BigDecimal remainingOutstandingPrincipal = loanAmount;
@@ -45,7 +43,19 @@ public class PlanService {
             BigDecimal interest = initialOutstandingPrincipal.multiply(monthlyRate).setScale(SHORT_SCALE, ROUNDING_MODE);
             BigDecimal principal = annuity.subtract(interest);
             remainingOutstandingPrincipal = initialOutstandingPrincipal.add(interest).subtract(annuity);
-            remainingOutstandingPrincipal = remainingOutstandingPrincipal.setScale(0, ROUNDING_MODE).compareTo(BigDecimal.ZERO) <= 0 ? BigDecimal.ZERO : remainingOutstandingPrincipal;
+
+            // Last payment entry special case
+            if (principal.compareTo(initialOutstandingPrincipal) > 0 ||
+                    remainingOutstandingPrincipal.setScale(0, ROUNDING_MODE).compareTo(BigDecimal.ZERO) <= 0) {
+                // Consideration in cases where due to rounding there is a mismatch by a couple cents
+                BigDecimal lastPayment = initialOutstandingPrincipal.add(interest).compareTo(annuity) > 0 ? annuity : initialOutstandingPrincipal.add(interest);
+                initialOutstandingPrincipal = lastPayment.subtract(interest);
+                principal = initialOutstandingPrincipal;
+                remainingOutstandingPrincipal = BigDecimal.ZERO;
+                result.add(new BorrowerPayment(lastPayment, periodDate, initialOutstandingPrincipal, interest, principal, remainingOutstandingPrincipal));
+                break;
+            }
+
             result.add(new BorrowerPayment(annuity, periodDate, initialOutstandingPrincipal, interest, principal, remainingOutstandingPrincipal));
             periodDate = periodDate.plusMonths(1);
         } while (result.size() < duration);
--- src/test/java/de/lendico/plangenerator/PlanServiceTest.java
+++ src/test/java/de/lendico/plangenerator/PlanServiceTest.java
@@ -35,7 +35,7 @@ class PlanServiceTest {
         List<BorrowerPayment> expectedPlan = Arrays.asList(
                 new BorrowerPayment(BigDecimal.valueOf(1680.57), LocalDate.of(2020, 1, 1), BigDecimal.valueOf(5000), BigDecimal.valueOf(20.83), BigDecimal.valueOf(1659.74), BigDecimal.valueOf(3340.26)),
                 new BorrowerPayment(BigDecimal.valueOf(1680.57), LocalDate.of(2020, 2, 1), BigDecimal.valueOf(3340.26), BigDecimal.valueOf(13.92), BigDecimal.valueOf(1666.65), BigDecimal.valueOf(1673.61)),
-                new BorrowerPayment(BigDecimal.valueOf(1680.57), LocalDate.of(2020, 3, 1), BigDecimal.valueOf(1673.61), BigDecimal.valueOf(6.97), BigDecimal.valueOf(1673.6), BigDecimal.ZERO)
+                new BorrowerPayment(BigDecimal.valueOf(1680.57), LocalDate.of(2020, 3, 1), BigDecimal.valueOf(1673.6), BigDecimal.valueOf(6.97), BigDecimal.valueOf(1673.6), BigDecimal.ZERO)
         );
 
         // when
@@ -44,6 +44,26 @@ class PlanServiceTest {
         // then
         assertEquals(duration, result.size());
         assertEqualBorrowPayments(expectedPlan, result);
+    }
+
+    @Test
+    public void generatePaymentPlan_differentLastPeriodPaymentAmount_lastPeriodGoodValues() {
+        // having
+        BigDecimal loanAmount = BigDecimal.valueOf(5000);
+        BigDecimal nominalRate = BigDecimal.valueOf(5.0);
+        Integer duration = 24;
+        LocalDate startDate = LocalDate.of(2020, 1, 1);
+
+        List<BorrowerPayment> expectedLastEntryPlan = Collections.singletonList(
+                new BorrowerPayment(BigDecimal.valueOf(219.28), LocalDate.of(2021, 12, 1), BigDecimal.valueOf(218.37), BigDecimal.valueOf(0.91), BigDecimal.valueOf(218.37), BigDecimal.ZERO)
+        );
+
+        // when
+        List<BorrowerPayment> result = planService.generatePaymentPlan(loanAmount, nominalRate, duration, startDate);
+
+        // then
+        assertEquals(duration, result.size());
+        assertEqualBorrowPayments(expectedLastEntryPlan, Collections.singletonList(result.get(23)));
     }
 
     @Test
@@ -67,7 +87,7 @@ class PlanServiceTest {
     }
 
     private void assertEqualBorrowPayments(List<BorrowerPayment> borrowerPayments, List<BorrowerPayment> results) {
-        for(int i =0; i<borrowerPayments.size(); i++){
+        for (int i = 0; i < borrowerPayments.size(); i++) {
             BorrowerPayment expected = borrowerPayments.get(i);
             BorrowerPayment result = results.get(i);
             assertEquals(0, expected.getBorrowerPaymentAmount().compareTo(result.getBorrowerPaymentAmount()));
